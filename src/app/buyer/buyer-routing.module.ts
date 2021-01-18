b8ca3e542487e5729a87d2e31eeae4d9ef194c8a
maksof-sami <61307728+maksof-sami@users.noreply.github.com>
2021-01-18 13:25:04 +0500

    module setup

---
+++ src/app/buyer/buyer-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BuyerComponent } from './buyer.component';
+
+const routes: Routes = [{ path: '', component: BuyerComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BuyerRoutingModule { }
