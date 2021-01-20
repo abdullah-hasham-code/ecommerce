3eff3cfd9012c3cdd1ee1829aff86a7598e0d302
maksof-sami <61307728+maksof-sami@users.noreply.github.com>
2021-01-20 19:15:26 +0500

    login

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -3,8 +3,13 @@ var sequelize = require('../sequelizeConfig').sequelizeConfig;
 exports.userlogin =async  (req, res) => {
     if(req.body.email=='') res.status(200).send({status : "FAIL",message: "Email is required"})
     if(req.body.password=='') res.status(200).send({status : "FAIL",message: "Password is required"})
-    query = "select * from tbl_users where email='"+req.body.email+"' and password='"+req.body.password+"'";
-    result = await sequelize.query(query,{ type: Sequelize.QueryTypes.SELECT });
-    if (result.length > 0) res.status(200).send({ status: "OK", message: "Record found!", data: result });
-    else res.status(200).send({ status: "FAIL", message: "No data found!" });
+    logincheck = "select * from tbl_users where email='"+req.body.email+"' and password='"+req.body.password+"'";
+    logincheckresult = await sequelize.query(logincheck,{ type: Sequelize.QueryTypes.SELECT });
+    if(logincheckresult.length>0) {
+        logincheck +=" and isVerified=1";
+        logincheckresult = await sequelize.query(logincheck,{ type: Sequelize.QueryTypes.SELECT });
+        if (logincheckresult.length > 0) res.status(200).send({ status: "OK", message: "Login successfully!", data: logincheckresult });
+    } else res.status(200).send({status: "FAIL", message: "Please verify your account first then login!"});
+    // if (result.length > 0) res.status(200).send({ status: "OK", message: "Login successfully!", data: result });
+    // else res.status(200).send({ status: "FAIL", message: "Sorry! this!" });
 }
